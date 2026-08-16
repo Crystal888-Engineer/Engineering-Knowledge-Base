f6ffde94bb8d34baee55ef55230a9c50ecdff626
Crystal Jacobs <conquela888@gmail.com>
2026-08-16 17:35:23 +0200

    Update Fundamentals with Variables.py

---
+++ Python/Fundamentals/variables.py
@@ -0,0 +1,12 @@
+# Variables are containers for storing data values. In Python, variables are created when you assign a value to them.
+
+print("My Name is Python")
+print("I am a programming language")
+print("I am easy to learn and use!")
+
+# This is a Variable
+name = "Crystal"
+language = "Python"
+print("My Name is", name)
+print(language, "is a programming language")
+print(language, "is easy to learn and use!")
