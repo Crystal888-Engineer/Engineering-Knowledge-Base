abc030741f4a1124f0a5365ea75b049ced929520
Crystal Jacobs <conquela888@gmail.com>
2026-08-17 15:59:53 +0200

    Python Data Types files

---
+++ Python/Data_Types/DataTypes.py
@@ -0,0 +1,17 @@
+# Basic Python Data Types
+# None = No Value
+# Single Value = int = 50, str = "Hello", float = 10.5, bool = True
+# Mutl Values = list = [1, 2, 3, 4, 5], tuple = (1, 2, 3, 4, 5), set = {1, 2, 3, 4, 5}, dict = {"name": "John", "age": 30}
+
+a = 10          # int
+b = 3.1         # float
+c = "Hello"     # str
+d = 'True'      # str
+e = "1234"      # str
+
+f = True        # Bool
+g = False       # Bool
+
+h = None        # NoneTypeL
+i = ""          # str = Blank
+j = " "         # str =Empty space
