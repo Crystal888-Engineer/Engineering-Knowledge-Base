be2a1904ea89fa587fbe60610ec2da129857a5a6
Crystal Jacobs <conquela888@gmail.com>
2026-08-17 15:40:06 +0200

    Add Input Code Sample File

---
+++ Python/Fundamentals/input.py
@@ -0,0 +1,7 @@
+# input() is a built-in Python function used to receive data from the user through the keyboard.
+
+# This will prompt the user to enter their name.
+name = input("Please enter your name: ")
+
+# This will print a greeting message with the user's name.
+print("Hello, " + name + "!")
